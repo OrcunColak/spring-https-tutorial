50b182316d24e6b0b4f65346d76e7a8f91f9299f
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-15 10:52:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTTPS
+The original idea is from  
+https://medium.com/@bubu.tripathy/securing-spring-boot-application-with-https-2ea75a86e4c7
---
+++ src/test/java/com/colak/springhttpstutorial/SpringHttpsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colak.springhttpstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
