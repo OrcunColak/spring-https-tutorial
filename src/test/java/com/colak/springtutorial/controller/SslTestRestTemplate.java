0e06e7e7ab5870ea989f23b474752ef780ff7c8b
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-22 10:28:32 +0300

    refactor test code for http client

--- src/test/java/com/colak/springtutorial/controller/SslTestRestTemplate.java
+++ src/test/java/com/colak/springtutorial/controller/SslTestRestTemplate.java
@@ -7,10 +7,10 @@ import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
 import org.apache.hc.core5.http.config.Registry;
 import org.apache.hc.core5.http.config.RegistryBuilder;
 import org.apache.hc.core5.ssl.SSLContexts;
-import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,33 +18,17 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class SslTestRestTemplate {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) throws Exception {
-        final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
 
-        final SSLContext sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-
-        final SSLConnectionSocketFactory sslConnectionSocketFactory =
-                new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-
-        final Registry<ConnectionSocketFactory> socketFactoryRegistry =
-                RegistryBuilder.<ConnectionSocketFactory>create()
-                        .register("https", sslConnectionSocketFactory)
-                        .register("http", new PlainConnectionSocketFactory())
-                        .build();
-
-        final BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
-
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .build();
+        CloseableHttpClient httpClient = createHttpClient();
 
         HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
 
@@ -52,5 +36,25 @@ public class SslTestRestTemplate {
 
         // Create a RestTemplate that uses custom request factory
         return restTemplateBuilder.requestFactory(() -> customRequestFactory).build();
+    }
+
+    private static CloseableHttpClient createHttpClient() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(new TrustAllStrategy())
+                .build();
+
+        SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                RegistryBuilder.<ConnectionSocketFactory>create()
+                        .register("https", sslConnectionSocketFactory)
+                        .register("http", new PlainConnectionSocketFactory())
+                        .build();
+
+        BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
+        return HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
     }
 }
